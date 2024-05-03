7a21cf40adeb35cc6b85d0c9cda89b9be29a3c67
justinziegler <justin.ziegler@gmail.com>
2024-05-03 16:26:42 -0700

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,13 @@
 var gulp = require('gulp');
 var nunjucksRender = require('gulp-nunjucks-render');
 
-function html() {
+module.exports.build = function() {
     gulp.task('default', function () {
-    return gulp.src('src/client/views/*.html')
-        .pipe(nunjucksRender({
-        path: ['src/client/views/'] // String or Array
-        }))
-        .pipe(gulp.dest('dist'));
+        return gulp.src('src/client/views/*.html')
+            .pipe(nunjucksRender({
+            path: ['src/client/views/'] // String or Array
+            }))
+            .pipe(gulp.dest('dist'));
     });
 }
 
-exports.build = html()
