033b9b9ba9fdfa4d6f109564857a81577fe038fa
justinziegler <justin.ziegler@gmail.com>
2024-05-06 13:27:47 -0700

    Gulp tweaks

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,51 @@
 var gulp = require('gulp');
 var nunjucksRender = require('gulp-nunjucks-render');
 
-module.exports.build = async function () {
-    gulp.task('default', function () {
-        return gulp.src('src/client/views/*.html')
-            .pipe(nunjucksRender({
-            path: ['src/client/views/'] // String or Array
-            }))
-            .pipe(gulp.dest('dist'));
-    });
+function copyAssets() {
+    return gulp.src(['src/client/assets/**/*'])
+        .pipe(gulp.dest('./build/client/assets/')) ;
 }
 
+function copyLib() {
+    return gulp.src('src/lib/**/*')
+        .pipe(gulp.dest('./build/lib/'));
+}
+
+function copyServer() {
+    return gulp.src('src/server/**/*')
+        .pipe(gulp.dest('./build/server/'));
+}
+
+function copyModules() {
+    return gulp.src('./node_modules/**/*')
+        .pipe(gulp.dest('./build/node_modules/'));
+}
+
+function copyViews() {
+    return gulp.src('src/client/views/*.html')
+        .pipe(nunjucksRender({
+            path: ['src/client/views/'] // String or Array
+        }))
+        .pipe(gulp.dest('build/client/views/'));
+}
+
+function copyPackageJson() {
+    return gulp.src("package.json").pipe(gulp.dest("./build"));
+}
+
+function moveAssetsCorrectly() {
+    return gulp.src('./build/client/views/assets/*').pipe(gulp.dest("./build/client/assets/"));
+}
+
+// module.exports.build = async function () {
+//     gulp.task('default', function () {
+//         return gulp.src('src/client/views/*.html')
+//             .pipe(nunjucksRender({
+//                 path: ['src/client/views/'] // String or Array
+//             }))
+//             .pipe(gulp.dest('build/client/views/'));
+//     });
+// }
+
+
+exports.build = gulp.series(copyAssets, copyLib, copyServer, copyModules, copyViews, copyPackageJson, moveAssetsCorrectly);
