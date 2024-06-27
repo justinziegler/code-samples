4ca0c01ed88bb4430fbe446738f23d2cccd45ed4
justinziegler <justin.ziegler@gmail.com>
2024-06-27 15:59:25 -0700

    Docker stuff

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@
 const Koa = require('koa');
 
 const app = new Koa();
-const homeRoutes = require('./src/server/homeRoutes');
+const homeRoutes = require('./server/homeRoutes');
 const path = require('path');
-const render = require('./src/lib/render');
+const render = require('./lib/render');
 const serve = require('koa-static');
 
 app.use(render);
-app.use(serve(path.resolve(__dirname, './src/client')));
+app.use(serve(path.resolve(__dirname, './client')));
 app
     .use(homeRoutes.routes())
     .use(homeRoutes.allowedMethods());
